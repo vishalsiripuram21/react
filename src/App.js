965ea51e5011ae14e37b6baff42c9e242b3e566b
Your Name <you@example.com>
2023-06-10 13:16:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./components/ExpensesItem";
+import ExpensesItem from "./components/ExpensesItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi this is React-app</h1>
+      <ExpensesItem></ExpensesItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpensesItem.js
@@ -0,0 +1,14 @@
+import "./ExpensesItem.css";
+function ExpensesItem() {
+  return (
+    <div className="Expense">
+      <div className="Expense">
+        <h2 id="first">March 21th 2021</h2>
+        <h2 id="second">Car insurance</h2>
+      </div>
+      <h2 id="third">$294.57</h2>
+    </div>
+  );
+}
+
+export default ExpensesItem;
