c6ffb542012b3262105f53fc96f9ce162a476679
Your Name <you@example.com>
2023-06-13 18:24:50 +0530

    modified content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -24,7 +24,6 @@ const NewExpense = (props) => {
       {addExpenseCond ? (
         <ExpenseForm
           onSaveNewDate={newData}
-          conditionCheck={addExpenseCond}
           func={fun}
         />
       ) : (
